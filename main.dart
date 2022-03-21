5a626b33a99f44d4ce2f22bc08bbef7d6e297b4b
cesargafer <cesargafer@users.noreply.github.com>
2022-03-21 16:30:52 -0600

    3.5. Home screen
    Agrega configuración inicial de home page mediante el uso de widget de video y Layouts en Flutter.

--- main.dart
+++ main.dart
@@ -1,58 +1,204 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+//
+
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = '3.5. Home screen';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Row(
-                  children:  <Widget>[
-                    Container(
-                      child: Text('Row dentro de un container', textAlign: TextAlign.center),
-                      color: Colors.blue,
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+      //VideoPlayerScreen(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.account_circle_rounded),
+          leadingWidth: 100, // default is 56
+        title: const Text('nature hub'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.favorite),
+            tooltip: 'Series',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Series <3')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.category),
+            tooltip: 'Categorias',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Categorias'),
                     ),
-                    Container(  // container
-                      child: Column(
-                        children: const <Widget>[
-                          Text('Usando Column'),
-                          Expanded(
-                            child: FittedBox(
-                              fit: BoxFit.contain, // otherwise the logo will be tiny
-                              child: FlutterLogo(),
-                            ),
-                          ),
-                        ],
+                    body: const Center(
+                      child: Text(
+                        'Licenciatura en Desarrollo de Sistemas Web Por: Cesar Simon Garcia Ferman - Código de estudiante: 220336404 -- Asesor: Lotzy Beatriz Fonseca Chiu',
+                        style: TextStyle(fontSize: 24),
                       ),
                     ),
-                    Stack(
-                      children: <Widget>[
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.red,
-                        ),
-                        Container(
-                          width: 90,
-                          height: 90,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          child: Text('Stack dentro de un container', textAlign: TextAlign.justify),
-                          width: 80,
-                          height: 80,
-                          color: Colors.blue,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),  //Text('Hello World'),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      ),
+      body: VideoPlayerScreen(),//Center(
+        //child: _widgetOptions.elementAt(_selectedIndex),
+      //),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fiber_new),
+            label: 'Estrenos',
+            backgroundColor: Colors.green,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+//
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar( --------------------------out
+        //title: const Text('Butterfly Video'), --------------------------out
+      //), --------------------------out
+      // Use a FutureBuilder to display a loading spinner while waiting for the
+      // VideoPlayerController to finish initializing.
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the VideoPlayerController has finished initialization, use
+            // the data it provides to limit the aspect ratio of the video.
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              // Use the VideoPlayer widget to display the video.
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            // If the VideoPlayerController is still initializing, show a
+            // loading spinner.
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
