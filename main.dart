dcda7475986f7b95e2d7ce535f8084467ed362cc
cesargafer <cesargafer@users.noreply.github.com>
2022-03-23 22:43:13 -0600

    3.6. Peticiones HTTP
    Implementan funciones y clases del paquete HTTP para consumir API pokeapi.
    Uso el visor de Json jsoneditoronline para revisión de estructura de la información obtenida.
    
    https://pokeapi.co/api/v2/pokemon/ditto
    https://jsoneditoronline.org/

--- main.dart
+++ main.dart
@@ -1,206 +1,95 @@
-import 'package:flutter/material.dart';
+
+
+/* Licenciatura en Desarrollo de
+Sistemas Web Por: Cesar Simon Garcia Ferman
+Código de estudiante: 220336404 Asesor: Lotzy Beatriz Fonseca Chiu */
+
+
 
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:http/http.dart' as http;
 
-//
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://pokeapi.co/api/v2/pokemon/ditto'));
 
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  
+  final String name;
+
+  const Album({
+    required this.name,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      name: json['name'],
+    );
+  }
+}
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = '3.5. Home screen';
-
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-      //VideoPlayerScreen(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.account_circle_rounded),
-          leadingWidth: 100, // default is 56
-        title: const Text('nature hub'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.favorite),
-            tooltip: 'Series',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Series <3')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.category),
-            tooltip: 'Categorias',
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Categorias'),
-                    ),
-                    body: const Center(
-                      child: Text(
-                        'Licenciatura en Desarrollo de Sistemas Web Por: Cesar Simon Garcia Ferman - Código de estudiante: 220336404 -- Asesor: Lotzy Beatriz Fonseca Chiu',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ),
-                  );
-                },
-              ));
-            },
-          ),
-        ],
-      ),
-      body: VideoPlayerScreen(),//Center(
-        //child: _widgetOptions.elementAt(_selectedIndex),
-      //),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fiber_new),
-            label: 'Estrenos',
-            backgroundColor: Colors.green,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-//
-
-class VideoPlayerScreen extends StatefulWidget {
-  const VideoPlayerScreen({Key? key}) : super(key: key);
-
-  @override
-  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
-}
-
-class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
+class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
 
   @override
   void initState() {
     super.initState();
-
-    // Create and store the VideoPlayerController. The VideoPlayerController
-    // offers several different constructors to play videos from assets, files,
-    // or the internet.
-    _controller = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-    );
-
-    // Initialize the controller and store the Future for later use.
-    _initializeVideoPlayerFuture = _controller.initialize();
-
-    // Use the controller to loop the video.
-    _controller.setLooping(true);
-  }
-
-  @override
-  void dispose() {
-    // Ensure disposing of the VideoPlayerController to free up resources.
-    _controller.dispose();
-
-    super.dispose();
+    futureAlbum = fetchAlbum();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //appBar: AppBar( --------------------------out
-        //title: const Text('Butterfly Video'), --------------------------out
-      //), --------------------------out
-      // Use a FutureBuilder to display a loading spinner while waiting for the
-      // VideoPlayerController to finish initializing.
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the VideoPlayerController has finished initialization, use
-            // the data it provides to limit the aspect ratio of the video.
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              // Use the VideoPlayer widget to display the video.
-              child: VideoPlayer(_controller),
-            );
-          } else {
-            // If the VideoPlayerController is still initializing, show a
-            // loading spinner.
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Wrap the play or pause in a call to `setState`. This ensures the
-          // correct icon is shown.
-          setState(() {
-            // If the video is playing, pause it.
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              // If the video is paused, play it.
-              _controller.play();
-            }
-          });
-        },
-        // Display the correct icon depending on the state of the player.
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Consumiendo pokeapi'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.name);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
       ),
     );
   }
 }
+
+
+
