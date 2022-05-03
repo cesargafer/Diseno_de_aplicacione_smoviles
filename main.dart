1c2dfb5d37ca00ec225798553f54d77f0c7c99c6
cesargafer <cesargafer@users.noreply.github.com>
2022-05-02 19:15:47 -0500

    Add main.dart y write_examples.dart
    Agrega write_examples.dart donde se define la clase WriteExamples StatefulWidget dentro del metodo build se define la llamada a la base de datos firebase con dailySpecialRef.set({'description': 'hello database'})

--- main.dart
+++ main.dart
@@ -1,95 +1,76 @@
-
-
-/* Licenciatura en Desarrollo de
-Sistemas Web Por: Cesar Simon Garcia Ferman
-Código de estudiante: 220336404 Asesor: Lotzy Beatriz Fonseca Chiu */
-
-
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:convert';
 
+import 'package:catalogo_peliculas/views/write_examples.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
-  final response = await http
-      .get(Uri.parse('https://pokeapi.co/api/v2/pokemon/ditto'));
+import 'firebase_config.dart';
+import 'views/read_examples.dart';
+//import 'tabs_page.dart';
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseConfig.platformOptions,
+    //runApp(const MyApp()
+  );
+
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Database Example',
+      home: MyHomePage(),
+    ),
+  );
 }
 
-class Album {
-  
-  final String name;
+class MyHomePage extends StatelessWidget {
+  final String title = "title"; // added
 
-  const Album({
-    required this.name,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      name: json['name'],
-    );
-  }
-}
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('Consumiendo pokeapi'),
+          title: const Text('title'),
         ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.name);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Check out our examples"),
+              SizedBox(
+                height: 6,
+                width: MediaQuery.of(context).size.width,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ReadExamples(),
+                    ),
+                  );
+                },
+                child: const Text("Read examples"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WriteExamples(),
+                      ),
+                    );
+                  },
+                  child: const Text("Write examples"))
+            ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
-
-
-
